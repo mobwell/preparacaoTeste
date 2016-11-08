03516fbd933952e2578567476972dfc771d311f6
Trainerpl022 <Trainerpl022@SLFRPL022PCFORM.ensino.atec.local>
2016-11-08 08:56:48 +0000

    Primeiro Commit

---
+++ src/hospital/Cirurgiao.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hospital;
+
+/**
+ *
+ * @author Trainerpl022
+ */
+public class Cirurgiao extends Medico {
+    
+    public void operar(Utente utente){}    
+}
